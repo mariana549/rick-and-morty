6160731e6a19cb2ca9b54c0b1a3f543cedd9d91f
mariana549 <marievaristo889@gmail.com>
2024-02-29 14:21:11 -0300

    refatorando informações que vão na api

--- src/scripts/objects/dados.js
+++ src/scripts/objects/dados.js
@@ -1,6 +1,16 @@
 const dados = {
-
-
+   image: "",
+   name: "",
+   status: "",
+   origin: "",
+   location: "",
+   setInfo(inforApi){
+      this.image = inforApi.image;
+      this.name = inforApi.name;
+      this.status = inforApi.status;
+      this.origin = inforApi.origin.name;
+      this.loocation = inforApi.loocation.name;
+   }
 }
 
-export { dados}
+export { dados }
